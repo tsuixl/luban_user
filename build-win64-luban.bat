428627f3290416614f3ae2023d800207e51b2e29
tsuixl <tsuixlcc@outlook.com>
2023-10-05 18:40:40 +0800

    增加Count字段

---
+++ build-win64-luban.bat
@@ -0,0 +1,9 @@
+@echo off
+echo Cleaning old build directory...
+rd /s /q win64
+
+echo Building project...
+dotnet build ./src/Luban/Luban.csproj -c Release -r win-x64 --self-contained true -o win64
+
+echo Build complete.
+pause
