8cc78b5e5953f5901e7721552ea21cd293d3e4d1
moonson618 <freedom1263@163.com>
2024-01-23 11:11:12 +0800

    location text

---
+++ gen_lua.bat
@@ -0,0 +1,14 @@
+D:\Moon\work\luban\luban_user\win64\Luban.exe ^
+-t client ^
+-c lua-bin ^
+-d sg-lua ^
+--conf D:\Moon\work\luban\luban_user\sample\locationtest\luban.conf ^
+--validationFailAsError ^
+-x outputCodeDir=D:\Moon\work\luban\luban_user\sample\locationtest\output\lua\code ^
+-x outputDataDir=D:\Moon\work\luban\luban_user\sample\locationtest\output\lua\data ^
+-x outputCustomDir=D:\Moon\work\luban\luban_user\sample\locationtest\custom ^
+-x outputAllType=D:\Moon\work\luban\luban_user\sample\locationtest\output\custom\all_type_output.txt ^
+-x locationFile=D:\Moon\work\luban\luban_user\sample\locationtest\output\custom\location.txt ^
+-x locationExportLanguage=all ^
+-x locationDefaultLanguage=zh
+pause
